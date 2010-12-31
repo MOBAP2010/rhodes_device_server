9684ee0136aef3d68b22aa0d6546bf2a4d04de98
Roman Simecek <roman.simecek@cyt.ch>
2010-12-31 09:54:48 +0100

    Rhodes server for device sync initial commited.

---
+++ spec/sources/device_spec.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__),'..','spec_helper')
+
+describe "Device" do
+  it_should_behave_like "SpecHelper"
+  
+  before(:each) do
+    setup_test_for Device,'testuser'
+  end
+  
+  it "should process Device query" do
+    pending
+  end
+  
+  it "should process Device create" do
+    pending
+  end
+  
+  it "should process Device update" do
+    pending
+  end
+  
+  it "should process Device delete" do
+    pending
+  end
+end
